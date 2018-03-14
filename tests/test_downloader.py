271c57723dc5efcbd1898c67226e19301330fe7c
Hubert Lacote <hubert.lacote@gmail.com>
2018-03-14 17:32:48 +0100

    Cleanup: conform to PEP8 -  E131 continuation line unaligned for indent

--- tests/test_downloader.py
+++ tests/test_downloader.py
@@ -59,7 +59,7 @@ def test_cookie_is_passed_in_headers(mock_requests):
         'accept': '*/*',
         'accept-encoding': 'gzip, deflate',
         'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 ' +
-            '(KHTML, like Gecko) Chrome/64.0.3282.167 Safari/537.36',
+        '(KHTML, like Gecko) Chrome/64.0.3282.167 Safari/537.36',
         'cookie': FAKE_COOKIE
     }
 
