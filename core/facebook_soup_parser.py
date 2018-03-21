a353277d44bbc914554a9c8fdee8630a0c0cecf9
Hubert Lacote <hubert.lacote@gmail.com>
2018-03-21 09:21:29 +0100

    facebook_soup_parser: add methods to parse timeline and reaction pages

--- core/facebook_soup_parser.py
+++ core/facebook_soup_parser.py
@@ -1,8 +1,12 @@
 from bs4 import BeautifulSoup
+from collections import namedtuple
 from collections import OrderedDict
 
 import logging
 import re
+
+
+TimelineResult = namedtuple('TimelineResult', ['articles', 'show_more_link'])
 
 
 class FacebookSoupParser:
@@ -201,3 +205,184 @@ class FacebookSoupParser:
                         {"Name": link.text}
 
         return friends_found
+
+    def parse_years_links_from_timeline_page(self, content):
+        """
+        >>> FacebookSoupParser().parse_years_links_from_timeline_page('''
+        ...     <div id="tlFeed">
+        ...         <a class="bn" href="badLink1">Mark</a>
+        ...         <a href="link1">2010</a>
+        ...         <a href="link2">2009</a>
+        ...         <a class="bn" href="badLink2">Dave</a>
+        ...     </div>''')
+        ['link1', 'link2']
+        >>> FacebookSoupParser().parse_years_links_from_timeline_page('''
+        ...     <input name="login" type="submit" value="Log In">''')
+        []
+        """
+
+        soup = BeautifulSoup(content, "lxml")
+
+        links_found = []
+
+        main_soup = soup.find(id="tlFeed")
+        if not main_soup:
+
+            login_found = soup.find("input", attrs={"name": "login"})
+            if login_found:
+                logging.error("Cookie expired or is invalid, login requested.")
+            else:
+                logging.error("Failed to parse timeline page")
+            return links_found
+
+        links_soup = main_soup.find_all('a')
+        for link in links_soup:
+            if "href" in link.attrs:
+                year_found = re.findall(r'\d{4}', link.text)
+                if year_found:
+                    links_found.append(link.attrs["href"])
+
+        return links_found
+
+    def parse_timeline_page(self, content):
+        """
+        >>> FacebookSoupParser().parse_timeline_page('''
+        ...     <div id="tlFeed">
+        ...         <div role="article">
+        ...             <abbr>13 May 2008 at 10:02</abbr>
+        ...             <span id="like_1">
+        ...                 <a href="/link1">Like</a>
+        ...                 <a href="/badLink1">React</a>
+        ...             </span>
+        ...         </div>
+        ...         <div role="article">
+        ...             <abbr>13 May 2008 at 10:25</abbr>
+        ...             <span id="like_2">
+        ...                 <a href="/link2">Like</a>
+        ...                 <a href="/badLink2">React</a>
+        ...             </span>
+        ...         </div>
+        ...         <div>
+        ...             <a href="/show_more_link">Show more</a>
+        ...         </div>
+        ...     </div>''')
+        TimelineResult(articles=OrderedDict([(1, '13 May 2008 at 10:02'), \
+(2, '13 May 2008 at 10:25')]), show_more_link='/show_more_link')
+        >>> FacebookSoupParser().parse_timeline_page('''
+        ...     <div id="tlFeed">
+        ...         <div role="article">
+        ...             <div role="article">
+        ...             </div>
+        ...             <abbr>13 May 2008 at 10:02</abbr>
+        ...             <span id="like_1">
+        ...                 <a href="/link1">Like</a>
+        ...                 <a href="/badLink1">React</a>
+        ...             </span>
+        ...         </div>
+        ...     </div>''')
+        TimelineResult(articles=OrderedDict([(1, '13 May 2008 at 10:02')]), \
+show_more_link='')
+        >>> FacebookSoupParser().parse_timeline_page('''
+        ...     <input name="login" type="submit" value="Log In">''')
+        """
+
+        soup = BeautifulSoup(content, "lxml")
+
+        main_soup = soup.find(id="tlFeed")
+        if not main_soup:
+
+            login_found = soup.find("input", attrs={"name": "login"})
+            if login_found:
+                logging.error("Cookie expired or is invalid, login requested.")
+            else:
+                logging.error("Failed to parse timeline page")
+            return None
+
+        articles_found = OrderedDict()
+
+        articles_soup = main_soup.find_all("div", attrs={"role": "article"})
+        for article in articles_soup:
+
+            abbr_tag = article.find("abbr")
+            if not abbr_tag:
+                logging.info("Skipping original article shared.")
+                continue
+
+            span_tag = article.find(id=re.compile("like_\d+"))
+            if not span_tag:
+                logging.info("Skipping article - no link for likes found.")
+                continue
+
+            article_id = int(re.findall(r'\d+', span_tag.attrs["id"])[0])
+
+            if article_id in articles_found:
+                # Can happen when adding photos to albums
+                logging.info(
+                    "Overriding date for article '{0}': "
+                    "old date: '{1}' - new date: '{2}'".format(
+                        article_id, articles_found[article_id], abbr_tag.text))
+            articles_found[article_id] = abbr_tag.text
+
+        show_more_link_tag = soup.find("a", string="Show more")
+        link_found = ""
+        if show_more_link_tag and "href" in show_more_link_tag.attrs:
+            link_found = show_more_link_tag.attrs["href"]
+
+        return TimelineResult(
+            articles=articles_found, show_more_link=link_found)
+
+    def parse_reaction_page(self, content):
+        """
+        >>> FacebookSoupParser().parse_reaction_page('''
+        ...     <div id="objects_container">
+        ...         <a role="button" href="/ufi/badLink">All 2</a>
+        ...         <a class="bn" href="/username1">Mark</a>
+        ...         <a class="bn" href="bad/Link1">Mark</a>
+        ...         <a class="bn" href="/username2">Paul</a>
+        ...         <a href="/a/mobile/friends/add_friend.php?id=123"></a>
+        ...         <a class="bn" href="badLink2">Dave</a>
+        ...         <a href="/ufi/reaction/profile/browser/fetch/?"></a>
+        ...     </div>''')
+        ['username1', 'username2']
+        >>> FacebookSoupParser().parse_reaction_page('''
+        ...     <div id="objects_container">
+        ...         <span>The page you requested cannot be displayed</span>
+        ...         <a href="/home.php?rand=852723744">Back to home</a>
+        ...     </div>''')
+        []
+        >>> FacebookSoupParser().parse_reaction_page('''
+        ...     <input name="login" type="submit" value="Log In">''')
+        """
+
+        soup = BeautifulSoup(content, "lxml")
+
+        usernames_found = []
+
+        main_soup = soup.find(id="objects_container")
+        if not main_soup:
+
+            login_found = soup.find("input", attrs={"name": "login"})
+            if login_found:
+                logging.error("Cookie expired or is invalid, login requested.")
+            else:
+                logging.error("Failed to parse timeline page")
+            return None
+
+        links_soup = main_soup.find_all(href=re.compile("^/.*"))
+        invalid_links = ["add_friend.php", "ufi/reaction", "home.php?"]
+        for link in links_soup:
+            if "role" not in link.attrs and \
+               "href" in link.attrs:
+
+                username = link.attrs["href"][1:]
+
+                is_invalid = False
+                for invalid_link in invalid_links:
+                    if invalid_link in username:
+                        is_invalid = True
+                        break
+
+                if username and not is_invalid:
+                    usernames_found.append(username)
+
+        return usernames_found
