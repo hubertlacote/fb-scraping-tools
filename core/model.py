f42763b56f99f1a4268caf5f307de9109ef1f334
Hubert Lacote <hubert.lacote@gmail.com>
2018-04-11 13:04:23 +0200

    model: add support for more Facebook time
    Facebook sometimes uses "5 mins", "1 min", "7 hrs", "1 hr", "Just now",
    "Yesterday at 19:34". Any of these were returning the time of parsing and
    an error could be seen in the logs.

--- core/model.py
+++ core/model.py
@@ -1,5 +1,5 @@
 from collections import OrderedDict
-from datetime import datetime
+from datetime import datetime, timedelta
 from dateutil import parser
 import copy
 import dataset
@@ -132,6 +132,64 @@ def process_data(times, user_infos,
     return parsed
 
 
+def parse_relative_time(date_str):
+    """
+    >>> parse_relative_time("5 mins")
+    datetime.timedelta(0, 300)
+    >>> parse_relative_time("1 min")
+    datetime.timedelta(0, 60)
+    >>> parse_relative_time("7 hrs")
+    datetime.timedelta(0, 25200)
+    >>> parse_relative_time("1 hr")
+    datetime.timedelta(0, 3600)
+    >>> parse_relative_time("Just now")
+    datetime.timedelta(0)
+    >>> parse_relative_time("Not a date")
+    """
+
+    found = re.findall(r'\d+ hr', date_str)
+    if found:
+        hours = int(found[0].split(" hr")[0])
+        return timedelta(hours=hours)
+
+    found = re.findall(r'\d+ min', date_str)
+    if found:
+        mins = int(found[0].split(" min")[0])
+        return timedelta(minutes=mins)
+
+    if "Just now" in date_str:
+        return timedelta(minutes=0)
+
+    return None
+
+
+def parse_fuzzy_time(date_str):
+    """
+    >>> parse_fuzzy_time("Yesterday at 19:34")
+    (datetime.time(19, 34), datetime.timedelta(1))
+    >>> parse_fuzzy_time("Today at 19:34")
+    (datetime.time(19, 34), datetime.timedelta(0))
+    >>> parse_fuzzy_time("Not a date")
+    """
+
+    try:
+        # fuzzy_with_tokens=True ignores yesterday / today when parsing
+        t = parser.parse(date_str, fuzzy_with_tokens=True)
+        parsed_time = t[0].time()
+        ignored = t[1]
+
+        delta = timedelta(seconds=0)
+        for ignored_part in ignored:
+            if "yesterday" in ignored_part.lower():
+                delta = timedelta(hours=24)
+
+        return tuple([parsed_time, delta])
+
+    except Exception:
+
+        return None
+
+
 def parse_date(date_str):
     """
     >>> parse_date("22 April 2011 at 20:34")
@@ -150,9 +208,25 @@ def parse_date(date_str):
             # We could directly parse all dates with parser,
             # but this allows to have logging only for incomplete dates
             return parser.parse(date_str)
+
         except Exception:
-            logging.error("Failed to parse date: {0}".format(date_str))
-            return datetime.now()
+
+            relative_time = parse_relative_time(date_str)
+            if relative_time:
+                return datetime.now() - relative_time
+
+            fuzzy_t = parse_fuzzy_time(date_str)
+            if fuzzy_t:
+                fuzzy_time = fuzzy_t[0]
+                delta = fuzzy_t[1]
+                return datetime.now().replace(
+                    hour=fuzzy_time.hour,
+                    minute=fuzzy_time.minute,
+                    second=fuzzy_time.second) - delta
+
+            else:
+                logging.error("Failed to parse date: {0}".format(date_str))
+                return datetime.now()
 
 
 def date_to_epoch(date):
